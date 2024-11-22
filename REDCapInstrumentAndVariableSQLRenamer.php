37f564341f5292a6d32e56218f10fb042c3c229a
bascome <eva.bascompte.moragas@vumc.org>
2024-11-22 09:34:06 -0600

    First upload

---
+++ REDCapInstrumentAndVariableSQLRenamer.php
@@ -0,0 +1,13 @@
+<?php
+namespace VUMC\REDCapInstrumentAndVariableSQLRenamer;
+
+use Exception;
+use REDCap;
+use ExternalModules\AbstractExternalModule;
+use ExternalModules\ExternalModules;
+
+class REDCapInstrumentAndVariableSQLRenamer extends AbstractExternalModule
+{
+
+}
+?>
